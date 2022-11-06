05f669bec188a1a1fa5e98e893534a332aad7e12
JACK <109951987+urk21cs1144@users.noreply.github.com>
2022-11-06 21:21:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
 from typing import List
 
-def selectionSort(array, size) -> List[int]:
-  # Write your code here
-
+def selectionSort(array, size) -> List[int]:  
+  for indexo in range(size-1):
+    small_index = indexo
+    for index in range(indexo + 1,size):
+      if array[index] < array[small_index]:
+        small_index = index
+    array[indexo], array[small_index] = array[small_index], array[indexo]
+  return array     
 # Do not change the following code
 input_data = input()
 data = []
